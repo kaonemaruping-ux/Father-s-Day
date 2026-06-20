d79edd0ec9ea2f2e97a83c26cd14224134dd1d12
kaonemaruping-ux <kaonemaruping@gmail.com>
2026-06-21 00:36:17 +0200

    Add files via upload

---
+++ FathersDayWebsite/script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+
+    document.getElementById("hiddenMessage").innerHTML =
+    "Happy Father's day to the best dad in the entire universe! Thank you so much for all that you've done for me. The efforts that you put in to make sure that we live a good live and that we have a good future, do not go unnoticed. I see all the sacrifices that you make for us and i appreciate you so much for that. Thank you for playing a role in shaping me to become the person that i am today, thank you for never giving up on me. I hope you have an amazing day today. May the Lord continue to shower you with lots and lots of blessing and watch over you at all time. I love you so much! from Fefe Kgatla. ";
+
+    document.getElementById("hiddenMessage2").innerHTML =
+    "Happy Father’s Day to the strongest, wisest, and most loving dad. Ke leboga tsotlhe tšeo o re diretšego tšona. Your hard work, guidance, and love keep our family strong every day. May Modimo continue to bless you with good health, peace, and happiness. Re a go rata Ntate! ❤️ from Tumi Kgatla." ;
+
+}
